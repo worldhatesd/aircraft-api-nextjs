7099ce728d193616f7b044abbc4d2256f13b2553
Deonte Palmer <ferfrink3577@gmail.com>
2024-06-16 04:46:41 -0500

    :recycle: refactor: Add API endpoint for main page

---
+++ src/app/pages/api/main.ts
@@ -0,0 +1,11 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+type ResponseData = {
+  message: string;
+};
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseData>
+) {
+  res.status(200).json({ message: "Hello from Next.js!" });
+}
